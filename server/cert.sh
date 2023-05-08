17110d798accd40e43bb681cc0c974e8d8d6a55c
Aemilian <aemilian.mayrhofer@protonmail.com>
2023-05-08 21:39:58 +0200

    created PROXY_USE_CERT env var

--- server/cert.sh
+++ server/cert.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-if [ "$NODE_ENV" != "production" ]
+if [ "$PROXY_USE_CERT" != "1" ]
 then
-    echo "Running in development mode."
+    echo "Running in HTTP only."
     exit 0
 fi
 
-echo "Running for production."
+echo "Running with letsencrypt and https."
+exit 0
 certbot --nginx --non-interactive --redirect --agree-tos -m $ADMIN_EMAIL -d $PROXY_DOMAIN
 
 echo "Done setting up certificate."
