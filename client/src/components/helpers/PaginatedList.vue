fccb78994171bb2ce170913cf179225a0e54b9eb
absurd turtle <absurdturtl3@gmail.com>
2023-09-21 16:07:17 +0200

    refactor playlistLoader

--- client/src/components/helpers/PaginatedList.vue
+++ client/src/components/helpers/PaginatedList.vue
@@ -1,26 +1,29 @@
 <template>
-    <div class="paginationWrapper">
-        <ul class="paginatedData">
-            <li v-for="p in paginatedData" :style="{background: colorPath? getObjectColor(p): '#ffffff00', ...itemStyle}" :key="p.id" @click="itemClick(p)">
-                <div class="columnOne" :style="columnOneStyle">
-                    <span v-if="columnOnePath">{{objectData(p, columnOnePath)}} </span>
-                </div>
-                <div class="columnTwo" :style="columnTwoStyle">
-                    <span v-if="columnTwoPath">{{objectData(p, columnTwoPath)}} </span>
-                </div>
-                <div class="columnThree" :style="columnThreeStyle">
-                    <span v-if="columnThreePath">{{objectData(p, columnThreePath)}}</span>
-                </div>
-            </li>
-        </ul>
-
-        <v-pagination
-            v-model="pageNumber"
-            :length="pageCount"
-            :total-visible="7"
-            class="navigator"
-        ></v-pagination>
-    </div>
+  <div class="paginationWrapper">
+    <ul class="paginatedData">
+      <li
+        v-for="p in paginatedData"
+        :style="{
+          background: colorPath ? getObjectColor(p) : '#ffffff00',
+          ...itemStyle,
+        }"
+        :key="p.id"
+        @click="itemClick(p)"
+      >
+        <div class="columnOne" :style="columnOneStyle">
+          <span v-if="columnOnePath">{{ objectData(p, columnOnePath) }} </span>
+        </div>
+        <div class="columnTwo" :style="columnTwoStyle">
+          <span v-if="columnTwoPath">{{ objectData(p, columnTwoPath) }} </span>
+        </div>
+        <div class="columnThree" :style="columnThreeStyle">
+          <span v-if="columnThreePath">{{
+            objectData(p, columnThreePath)
+          }}</span>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 <script lang="js">
 export default {
@@ -113,41 +116,41 @@ export default {
 }
 </script>
 <style scoped>
+.paginationWrapper {
+  display: grid;
 
-    .paginationWrapper{
-        display: grid;
-
-        grid-template: "content" 1fr
-                        "navigator" 3rem/auto
-    }
-    #navigator{
-        grid-area: navigator;      
-    }
-    .paginatedData{
-        grid-area: content;
-        height: 25rem;
-        overflow: auto;
-    }
-    ul{
-        list-style-type: none;
-    }
-    li{
-        display: grid;
-        grid-template: "columnOne columnTwo columnThree" /1fr 1fr 1fr
-    }
-    .columnOne{
-        grid-area: "columnOne"
-    }
-    .columnTwo{
-        grid-area: "columnTwo"
-    }
-    .columnThree{
-        grid-area: "columnThree"
-    }
-    li:hover {
-        cursor: pointer; 
-    }
-    li:active {
-        background-color: #0d676d;
-    }
+  grid-template:
+    "content" 1fr
+    "navigator" 3rem / auto;
+}
+#navigator {
+  grid-area: navigator;
+}
+.paginatedData {
+  grid-area: content;
+  height: 25rem;
+  overflow: auto;
+}
+ul {
+  list-style-type: none;
+}
+li {
+  display: grid;
+  grid-template: "columnOne columnTwo columnThree" /1fr 1fr 1fr;
+}
+.columnOne {
+  grid-area: "columnOne";
+}
+.columnTwo {
+  grid-area: "columnTwo";
+}
+.columnThree {
+  grid-area: "columnThree";
+}
+li:hover {
+  cursor: pointer;
+}
+li:active {
+  background-color: #0d676d;
+}
 </style>
