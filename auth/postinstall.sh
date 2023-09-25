eab6999bbc2f5a74ebc512995b6492ed983f25a3
Aemilian Mayrhofer <aemilian.mayrhofer@gmail.com>
2023-09-25 16:51:24 +0200

    removed client and user authentication from auth service

--- auth/postinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ../libraries/auth-tools
-npm install
-npm link
-cd ../../auth
-npm link auth-tools
--- auth/settings.js
+++ auth/settings.js
@@ -1,11 +1,6 @@
 require('dotenv').config()
 
 module.exports = {
-    ARANGO_HOST: process.env.ARANGODB_HOST ?? "arangodb",
-    ARANGO_PORT: process.env.ARANGODB_PORT ?? "8529",
-    ARANGO_DB: process.env.ARANGODB_HOST ?? "OOT",
-    ARANGO_USER: process.env.ARANGODB_USER,
-    getArangoPassword: () => process.env.ARANGODB_PASSWORD,
     PROXY_URI: process.env.PROXY_URI,
     SPOTIFY_SCOPE: process.env.SPOTIFY_SCOPE,
     SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
