7aa825065e884c8276c04ccd54b1cf1c3c38f047
Aemilian Mayrhofer <aemilian.mayrhofer@gmail.com>
2023-10-01 22:32:21 +0200

    added default spotify scope config

--- packages/settings/src/sections/spotifySection.ts
+++ packages/settings/src/sections/spotifySection.ts
@@ -10,7 +10,7 @@ class SpotifySection extends Section {
 const spotifySection = new SpotifySection("Spotify", [
     new Field("clientId", Types.string, "Spotify Client ID for Client Credentials Authentication Flow", {envName: "SPOTIFY_CLIENT_ID"}),
     new Field("clientSecret", Types.string, "Spotify Client Secret for Client Credentials Authentication Flow", {envName: "SPOTIFY_CLIENT_SECRET", sensitive: true}),
-    new Field("scope", Types.string, "Required scope for Spotify OAUTH", {envName: "SPOTIFY_SCOPE"}),
+    new Field("scope", Types.string, "Required scope for Spotify OAUTH", {envName: "SPOTIFY_SCOPE", default: "user-read-private user-read-email"}),
 ])
 
 export default spotifySection
