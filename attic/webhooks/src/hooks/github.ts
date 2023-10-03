bb64025e1ca807cf87bf8deb7783d207615d82db
Aemilian Mayrhofer <aemilian.mayrhofer@gmail.com>
2023-10-03 18:18:37 +0200

    Added CICD tests for the webhook service

--- attic/webhooks/src/hooks/github.ts
+++ attic/webhooks/src/hooks/github.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import * as crypto from "crypto"
 import { githubSecret } from '../settings.js'
 import { spawnChild } from '../child.js'
+import fs from 'fs/promises'
 
 
 type HookHandler = (request: express.Request) => void
@@ -22,31 +23,24 @@ const verifySignature = (req: express.Request) => {
 
 const pingHook: HookHandler = (_) => console.log('GitHub sent the ping event')
 
-const issueHook: HookHandler = (request) => {
-  const data = request.body
-  const action = data.action
-  if (action === 'opened') {
-    console.log(`An issue was opened with this title: ${data.issue.title}`)
-  } else if (action === 'closed') {
-    console.log(`An issue was closed by ${data.issue.user.login}`)
-  } else {
-    console.log(`Unhandled action for the issue event: ${action}`)
-  }
-}
-
 const deployHook: HookHandler = (_) => {
   spawnChild('RestartProd', './start-prod.sh', [], {cwd: '../..'}).catch(
     err =>  {console.error("async error:\n" + err)}
   )
 }
 
-deployHook(null)
-const hookMap: {[k: string]: HookHandler} = {
-  ping: pingHook,
-  issues: issueHook
+const testHook: HookHandler = (_) => {
+  fs.writeFile('testing/success', '')
+  console.log("Test Event was called: Successful")
 }
 
-
+const standardHookMap: {[k: string]: HookHandler} = {
+  ping: pingHook
+}
+const customHookMap: {[k: string]: HookHandler} = {
+  deploy: deployHook,
+  test: testHook
+}
 
 export const githubRouter = express.Router()
 
@@ -57,8 +51,18 @@ githubRouter.post('/webhook', express.json({type: 'application/json'}), (request
   const githubEvent = request.headers['x-github-event']
 
   if (Array.isArray(githubEvent)) console.warn(`Unsupported array of events: [${githubEvent}]`)
-  else if (githubEvent in hookMap) hookMap[githubEvent](request)
-  else console.warn(`Unhandled event: ${githubEvent}`)
+  else if (githubEvent in standardHookMap) standardHookMap[githubEvent](request)
+  else if (githubEvent !== undefined) console.warn(`Unhandled event: ${githubEvent}`)
+  else {
+    try {
+      const data = request.body
+      const event = data.event
+      if (event in customHookMap) customHookMap[event](request)
+      else console.warn(`Unhandled custom event: ${event}`)
+    } catch (error) {
+      console.warn(`Unknown request: ${request}\nError: ${error}`)
+    }
+  }
 })
 
 
