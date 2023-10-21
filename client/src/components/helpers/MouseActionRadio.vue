96e1ed1de0ddd76c666b5368b6f9cde34b19cd1b
absurd turtle <absurdturtl3@gmail.com>
2023-10-21 19:15:06 +0200

    fixed: refresh on radio button click

--- client/src/components/helpers/MouseActionRadio.vue
+++ client/src/components/helpers/MouseActionRadio.vue
@@ -1,89 +1,67 @@
 <template>
-  <form action="" id="MouseActionRadio">
-    <button class="icon-btn clickthrough tooltip">
+  <div class="click-actions">
+    <button
+      class="icon-btn tooltip"
+      :class="{ selected: activeMode === 'expand' }"
+      @click="expandClickHandler"
+    >
       <label id="expand-button" for="expand">
-        <input
-          type="radio"
-          id="expand"
-          v-model="activeMode"
-          value="expand"
-          v-on:change="setActiveMode(activeMode)"
-        />
         <v-icon class="icon" color="white" name="md-add" />
         <span class="tooltiptext">EXPAND</span>
       </label>
     </button>
-    <button class="icon-btn clickthrough tooltip">
+    <button
+      class="icon-btn tooltip"
+      :class="{ selected: activeMode === 'collapse' }"
+      @click="collapseClickHandler"
+    >
       <label id="collapse-button" for="collapse">
-        <input
-          type="radio"
-          id="collapse"
-          v-model="activeMode"
-          value="collapse"
-          v-on:change="setActiveMode(activeMode)"
-        />
         <v-icon class="icon" color="white" name="md-remove" />
         <span class="tooltiptext">COLLAPSE</span>
       </label>
     </button>
-    <button class="icon-btn clickthrough tooltip">
+    <button
+      class="icon-btn tooltip"
+      :class="{ selected: activeMode === 'explore' }"
+      @click="exploreClickHandler"
+    >
       <label id="explore-button" for="explore">
-        <input
-          type="radio"
-          id="explore"
-          v-model="activeMode"
-          value="explore"
-          v-on:change="setActiveMode(activeMode)"
-        />
         <v-icon class="icon" color="white" name="md-musicnote" />
         <span class="tooltiptext">EXPLORE</span>
       </label>
     </button>
-  </form>
+  </div>
 </template>
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapState } from "vuex";
 export default {
-  data: () => {
-    return {
-      activeMode: "expand",
-    };
+  computed: {
+    ...mapState({
+      activeMode: (state) => state.activeMode,
+    }),
   },
   methods: {
     ...mapActions(["setActiveMode"]),
+    exploreClickHandler: function () {
+      this.setActiveMode("explore");
+    },
+    expandClickHandler: function () {
+      this.setActiveMode("expand");
+    },
+    collapseClickHandler: function () {
+      this.setActiveMode("collapse");
+    },
   },
 };
 </script>
 <style scoped>
-#MouseActionRadio {
+.click-actions {
   display: flex;
   gap: 0.5rem;
   flex-direction: column;
 }
 
-[type="radio"] {
-  position: absolute;
-  opacity: 0;
-  width: 0;
-  height: 0;
-  padding: 0;
-  margin: 0;
-}
-
-[type="radio"]:checked + .icon {
-  background: #2d9cdb;
-}
-
-[type="radio"]:hover + .icon {
-  background: linear-gradient(180deg, #2d9cdb 0%, #56ccf2 100%);
-}
-
-.clickthrough {
-  pointer-events: none;
-}
-
-.clickthrough label {
-  pointer-events: auto;
-  cursor: pointer;
+.selected {
+  background: #2d9cdbaa;
 }
 </style>
